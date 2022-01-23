20383e3123b5de56149e46d0ba14d2f9e102215d
obopodo <bpodolnyi@gmail.com>
2022-01-23 23:31:41 +0200

    knn algorithm

--- hw1/knn/nearest_neighbors.py
+++ hw1/knn/nearest_neighbors.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import numpy as np
 
 from knn.distances import euclidean_distance, cosine_distance
@@ -8,13 +9,15 @@ def get_best_ranks(ranks, top, axis=1, return_ranks=False):
 
 
 class NearestNeighborsFinder:
-    def __init__(self, n_neighbors, metric="euclidean"):
+    def __init__(self, n_neighbors: int, metric: str = "euclidean") -> None:
         self.n_neighbors = n_neighbors
 
         if metric == "euclidean":
             self._metric_func = euclidean_distance
+            self.less_is_better = True
         elif metric == "cosine":
             self._metric_func = cosine_distance
+            self.less_is_better = False
         else:
             raise ValueError("Metric is not supported", metric)
         self.metric = metric
@@ -23,5 +26,15 @@ class NearestNeighborsFinder:
         self._X = X
         return self
 
-    def kneighbors(self, X, return_distance=False):
-        raise NotImplementedError()
+    def kneighbors(self, X, return_distance=False) -> Tuple[np.array]:
+        distances = self._metric_func(X, self._X)
+        sorted_indices = np.argsort(distances)
+        if self.less_is_better:
+            neighbours_indices = sorted_indices[:, :self.n_neighbors] # smaller distance -> first n indices
+        else:
+            neighbours_indices = sorted_indices[:, -self.n_neighbors:] # higher similarity -> last n indices
+
+        if return_distance:
+            return distances, neighbours_indices
+        else:
+            return neighbours_indices
